66cd3d7fb0a72bb09727fe3a0465db391a7a9e1b
Mikail Thoriq <xenon.ext000@gmail.com>
2022-02-20 14:14:13 +0700

    fix typo

--- absensi-production/header.php
+++ absensi-production/header.php
@@ -4,4 +4,4 @@
 <link rel="stylesheet" type="text/css" href="css/homepage.css">
 <link rel="shortcut icon" type="image/png" href="https://ik.imagekit.io/riqcdn/ks-funtasticthree-2022-web-assets/__d2URh5_cY.png"/>
 <script type="text/javascript" src="jquery/jquery.min.js"></script>
-<script type="text/javascript" src="js/bootstrap.min.js"></scrip
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
